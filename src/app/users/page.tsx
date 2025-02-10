efa26e31cbd9c81f3244f62dd84e55b49ca3618d
Uliia17 <45678julia@gmail.com>
2025-02-10 14:32:19 +0200

    lesson3-3

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,9 +1,10 @@
 const UsersPage = async () => {
-    const users = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await fetch('https://jsonplaceholder.typicode.com/users', {next: {revalidate: 5}})
                 .then(value => value.json());
 
     return (
         <div>
+            <h2>{Date.now()}</h2>
             {
                 users.map((user: { id:number, name:string }) => (<div key={user.id}>{user.name}</div>))
             }
