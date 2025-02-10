05dc3e2eaf9493b3e9114ed0d85a42af768420de
Uliia17 <45678julia@gmail.com>
2025-02-10 12:36:27 +0200

    [update]: master

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,15 +18,16 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+      <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      {children}
+      <hr/>
       </body>
-    </html>
+      </html>
   );
 }
